096eb3420dc23b888459e5e114fafd6da2407407
niyouxiaorouwanmang <105407048+niyouxiaorouwanmang@users.noreply.github.com>
2022-05-12 11:59:29 +0800

    Update config.rev_g.h

--- keyboard/lot60-ble/common/config.rev_g.h
+++ keyboard/lot60-ble/common/config.rev_g.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define PRODUCT "Lot60.G" /* 硬件名称，用于蓝牙显示 */
+#define PRODUCT "Fly 60" /* 硬件名称，用于蓝牙显示 */
 #define DEVICE_VER 0x0003 /* 硬件版本 */
 
 #define LED_CAPS 19
@@ -16,8 +16,8 @@
 
 #define BATTERY_ADC_PIN NRF_SAADC_INPUT_AIN0
 
-static const uint8_t row_pin_array[MATRIX_ROWS] = { 25, 28, 29, 30, 26 };
-static const uint8_t column_pin_array[MATRIX_COLS] = { 16, 15, 14, 13, 12, 11, 27, 3, 4, 5, 6, 7, 8, 9 };
+static const uint8_t row_pin_array[MATRIX_ROWS] = { 27, 26, 30, 29, 25 };
+static const uint8_t column_pin_array[MATRIX_COLS] = { 28, 11, 12, 13, 14, 16, 15, 9, 8, 7, 6, 5, 4, 3 };
 
 #define ROW_IN // 二极管方向是从COL->ROW
 
